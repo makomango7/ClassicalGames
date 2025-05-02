5c1ab62f15968de11ec8bbb1dca3d24d0eba6fe5
Artur <makomango7@gmail.com>
2025-05-02 12:26:49 +0500

    View logic

--- TikiTakiScreen/MainWindow.xaml.cs
+++ TikiTakiScreen/MainWindow.xaml.cs
@@ -18,13 +18,15 @@ public partial class MainWindow : Window
 
         game = new Game();
        
-        int size = 4;      
+        int size = 3;      
         int cellSize = 50;
 
         Grid myGrid = new Grid();
         myGrid.Width = size * cellSize;
         myGrid.Height = size * cellSize;
         myGrid.ShowGridLines = true;
+        myGrid.Name = "windowGrid";
+        RegisterName("windowGrid", myGrid);
         myGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
         myGrid.VerticalAlignment= VerticalAlignment.Stretch;
 
@@ -50,12 +52,50 @@ public partial class MainWindow : Window
         for(int i = 0; i < size * size; i++)
         {
             Button button = new Button();
+            button.Click += Button_Click;
             button.Content = "-";
             Grid.SetRow(button, CommonExtensions.GetXFromIndex(i, size));
             Grid.SetColumn(button, CommonExtensions.GetYFromIndex(i, size));
             myGrid.Children.Add(button);
         }
 
-        this.Content = myGrid;
+        mainStack.Children.Add(myGrid);
     }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if(game.IsFinished)
+        {
+            return;
+        }
+        
+        int x = Grid.GetColumn(sender as Button);
+        int y = Grid.GetRow(sender as Button);
+        var res = game.MakeATurn(x, y);
+
+        txtStatus.Text = "Status: " + res.ToString(); 
+
+        if(res != TurnState.WrongInput)
+        {
+            UpdateCells();
+        }
+    }
+
+    public void UpdateCells()
+    {
+        var griddie = this.FindName("windowGrid") as Grid;
+
+        for (int i = 0; i < game.Size * game.Size; i++)
+        {
+            var x = CommonExtensions.GetXFromIndex(i, game.Size);
+            var y = CommonExtensions.GetYFromIndex(i, game.Size);
+
+            var child = griddie.Children
+                .Cast<UIElement>()
+                .First(elem => Grid.GetRow(elem) == x && Grid.GetColumn(elem) == y) as Button;
+
+            child.Content = game.Arr[i];
+        }
+    }
+
 }
